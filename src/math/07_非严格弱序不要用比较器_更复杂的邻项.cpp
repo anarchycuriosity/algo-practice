f7e4550950d55028132d92fc06c7c244c0b7b76c
Curiosity <2681446651@qq.com>
2026-08-20 13:16:03 +0800

    二元组字符序比较逻辑,比较键

--- src/math/07_非严格弱序不要用比较器_更复杂的邻项.cpp
+++ src/math/07_非严格弱序不要用比较器_更复杂的邻项.cpp
@@ -1,36 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 struct Arr
 {
-    int x, y;
+    ll x, y;
 };
+ll get_small_key(Arr& arr) { return min(arr.x, arr.y); }
+ll get_big_key(Arr& arr) { return max(arr.x, arr.y); }
 bool comp(Arr& arr1, Arr& arr2)  // 需要交换就返回true
 // 好吧准确来说应该是谁应该在前面,默认arr1在前面
 {
     // 不交换
-    // int cnt1 = (arr1.x > arr2.x) + (arr1.x > arr2.y) + (arr1.y > arr2.x) + (arr1.y > arr2.y);
-    // int cnt2 = (arr2.x > arr1.x) + (arr2.x > arr1.y) + (arr2.y > arr1.x) + (arr2.y > arr1.y);
+    // ll cnt1 = (arr1.x > arr2.x) + (arr1.x > arr2.y) + (arr1.y > arr2.x) + (arr1.y > arr2.y);
+    // ll cnt2 = (arr2.x > arr1.x) + (arr2.x > arr1.y) + (arr2.y > arr1.x) + (arr2.y > arr1.y);
     // return (cnt1 > cnt2);
-    return max(arr1.x, arr1.y) > max(arr2.x, arr2.y);
+    if (get_small_key(arr1) < get_small_key(arr2))
+    {
+        return true;
+    }
+    else if (get_small_key(arr1) == get_small_key(arr2))
+    {
+        if (get_big_key(arr1) < get_big_key(arr2))
+        {
+            return true;
+        }
+        return false;
+    }
+    return false;
 }
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    vector<vector<int>> ans;
-    int t;
+    vector<vector<ll>> ans;
+    ll t;
     cin >> t;
-    int tt = t;
+    ll tt = t;
 
     while (tt--)
     {
-        int n;
+        ll n;
         cin >> n;
-        int nn = n;
+        ll nn = n;
         vector<Arr> arrs;
         while (nn--)
         {
-            int x, y;
+            ll x, y;
             cin >> x >> y;
             // Arr arr;
             // arr.x = x;
@@ -38,27 +53,18 @@ int main()
             // arrs.push_back(arr);
             arrs.push_back({x, y});
         }
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = n - 1; j > 0; j--)
-            {
-                if (comp(arrs[j - 1], arrs[j]))
-                {
-                    swap(arrs[j - 1], arrs[j]);
-                }
-            }
-        }
-        vector<int> tmp;
-        for (int i = 0; i < arrs.size(); i++)
+        sort(arrs.begin(), arrs.end(), comp);
+        vector<ll> tmp;
+        for (ll i = 0; i < arrs.size(); i++)
         {
             tmp.push_back(arrs[i].x);
             tmp.push_back(arrs[i].y);
         }
         ans.push_back(tmp);
     }
-    for (int i = 0; i < t; i++)
+    for (ll i = 0; i < t; i++)
     {
-        for (int j = 0; j < ans[i].size(); j++)
+        for (ll j = 0; j < ans[i].size(); j++)
         {
             cout << ans[i][j] << " ";
         }
